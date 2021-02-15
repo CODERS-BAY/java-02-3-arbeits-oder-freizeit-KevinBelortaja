dab7887131713a05e6d9dc7b2b018fa5fa993d52
Kevin Belortaja <kevin.belortaja@gmail.com>
2021-02-15 10:30:52 +0100

    Add exercise 2.3

--- .github/src/Main.java
+++ .github/src/Main.java
@@ -8,7 +8,11 @@ public class Main {
         System.out.println("Aktuelle Uhrzeit eingeben:");
         int number = scan.nextInt();
 
-        if (number >= 8 && number <= 16) {
+        if (number == 12) {
+            System.out.println("Mittagspause!");
+        }
+
+        else if (number >= 8 && number <= 16) {
             System.out.println("Die Stunde liegt in der Arbeitszeit");
         } else {
             System.out.println("Die Stunde liegt nicht in der Arbeitszeit");
